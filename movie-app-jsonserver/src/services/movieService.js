df5ce71455da432db633c63b7c522f71aaee82e3
zoharby1117 <zoharb23@gmail.com>
2026-09-15 16:05:23 -0400

    Update movieService.js

--- movie-app-jsonserver/src/services/movieService.js
+++ movie-app-jsonserver/src/services/movieService.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const MOVIE_URL = "http://localhost:3001/movies";
+const MOVIE_URL = "http://localhost:3001/api/movies";//global movies url
 
 const getAllMovies = async () => {
   const response = await axios.get(MOVIE_URL);
