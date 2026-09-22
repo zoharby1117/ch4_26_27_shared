8454aa482009506afd0c911442fe9a937239cd7c
zoharby1117 <zoharb23@gmail.com>
2026-09-22 15:45:11 -0400

    Update vite.config.js
    For last time: to deploy,, just run npm run build in frontend, copy it into backend, then push to github repo. Render will automatically update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  //we want it to get from 3001 during dev, not 5173. 5173 is frontend
+  server:{
+    proxy:{
+      "/api":{
+        target:"http://localhost:3001",
+      }
+    }
+  }
 })
